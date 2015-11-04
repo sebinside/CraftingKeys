8dbffd4dc973ec52676f19449e6d62c2062fada9
tosta3000 <to.sta@icloud.com>
2015-11-04 19:50:43 +0100

    Just removed a comment.

--- src/main/java/de/skate702/craftingkeys/util/LanguageLocalizer.java
+++ src/main/java/de/skate702/craftingkeys/util/LanguageLocalizer.java
@@ -2,9 +2,6 @@ package de.skate702.craftingkeys.util;
 
 import net.minecraft.util.StatCollector;
 
-/**
- * Created by Tobi on 04.11.2015.
- */
 public class LanguageLocalizer {
     //TODO: Finish localizer
     public static String localizeIfPossible(String unloc){
