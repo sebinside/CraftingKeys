a6db549ffed9424eac85017172f280464587c44b
Marcel <MTRNord@users.noreply.github.com>
2016-11-28 11:40:32 +0100

    Move 1.11 stuff to comment

--- src/main/java/de/skate702/craftingkeys/manager/BrewingManager.java
+++ src/main/java/de/skate702/craftingkeys/manager/BrewingManager.java
@@ -36,7 +36,8 @@ public class BrewingManager extends ContainerManager {
 
     @Override
     protected int specificKeyToSlotIndex() {
-        return mapKeyToSlot(4, 3, -1, 0, 1, 2, -1, -1, -1);
+        //NEW_1.11 return mapKeyToSlot(4, 3, -1, 0, 1, 2, -1, -1, -1);
+        return mapKeyToSlot(-1, 3, -1, 0, 1, 2, -1, -1, -1);
     }
 
     @Override
